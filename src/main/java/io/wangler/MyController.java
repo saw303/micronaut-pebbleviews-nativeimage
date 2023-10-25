966258333e342e3368f8d149e82fb85ea5b37a90
Silvio Wangler <71073+saw303@users.noreply.github.com>
2023-10-25 20:17:13 +0200

    Make Pebble version work

--- src/main/java/io/wangler/MyController.java
+++ src/main/java/io/wangler/MyController.java
@@ -3,6 +3,8 @@ package io.wangler;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.views.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,7 +13,9 @@ import java.util.Map;
 
 @Controller("/views")
 public class MyController {
-    
+
+    private static final Logger log = LoggerFactory.getLogger(MyController.class);
+
     @Get("/pebble2")
     public ModelAndView<Map<String, Object>> pebble2() {
         Map<String, Object> map = new HashMap<>();
@@ -21,6 +25,7 @@ public class MyController {
         numbers.add(3);
         numbers.add(4);
         map.put("numbers", numbers);
+        log.info("Hell yeah, Pebble numbers {}", numbers);
         return new ModelAndView<>("pebble/home2", map);
     }
 }
